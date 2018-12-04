fa4750595d090d71b2ba4a4e4387a936511e5484
Elsheikh <telsheikh@MTVL1604966ee.local>
2018-12-03 20:44:04 -0800

    Updates

---
+++ precache-manifest.c32770bdf325e9332646ecf7d26ef346.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "766603df4bae7607e9e4e8be4dcc26c6",
+    "url": "www.beanns.com/static/media/github.766603df.svg"
+  },
+  {
+    "revision": "30d53f667d1f91a77446",
+    "url": "www.beanns.com/static/css/main.1cf1f539.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "www.beanns.com/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "66aaf31adc2d0191fe1e",
+    "url": "www.beanns.com/static/js/1.66aaf31a.chunk.js"
+  },
+  {
+    "revision": "755f8ef6918d53b1615b",
+    "url": "www.beanns.com/static/js/runtime~main.755f8ef6.js"
+  },
+  {
+    "revision": "9aa4f7b7e63f490a4b71e9e172cd973f",
+    "url": "www.beanns.com/static/media/icons.9aa4f7b7.svg"
+  },
+  {
+    "revision": "5608be7aa8351c27bc295136efdcf3fa",
+    "url": "www.beanns.com/static/media/linkedin.5608be7a.svg"
+  },
+  {
+    "revision": "30d53f667d1f91a77446",
+    "url": "www.beanns.com/static/js/main.30d53f66.chunk.js"
+  },
+  {
+    "revision": "eda2e27876aa4693a67d03fd78a83367",
+    "url": "www.beanns.com/static/media/twitter.eda2e278.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "www.beanns.com/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "www.beanns.com/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "www.beanns.com/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "www.beanns.com/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "66aaf31adc2d0191fe1e",
+    "url": "www.beanns.com/static/css/1.75b18283.chunk.css"
+  },
+  {
+    "revision": "da043fdce962a3710bb0ba04c7ce25c8",
+    "url": "www.beanns.com/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/beanns/precache-manifest.949a656ab99bba0d74e6880113b0a555.js"
+  "www.beanns.com/precache-manifest.c32770bdf325e9332646ecf7d26ef346.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/beanns/index.html", {
+workbox.routing.registerNavigationRoute("www.beanns.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

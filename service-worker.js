4f2a4fa260efa3e433300bf22514579fb5497ebd
Elsheikh <telsheikh@MTVL1604966ee.local>
2018-12-03 20:45:28 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "www.beanns.com/precache-manifest.c32770bdf325e9332646ecf7d26ef346.js"
+  "/beanns/precache-manifest.d54ae0730cb6826dd814bb1d61481a6d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("www.beanns.com/index.html", {
+workbox.routing.registerNavigationRoute("/beanns/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

d052abfad0ad38dd1d1288bf952eac694820c22d
Elsheikh <telsheikh@MTVL1604966ee.local>
2018-12-06 00:12:27 -0800

    Updates

---
+++ precache-manifest.310b625709d7fd11fa0178fed318f841.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "766603df4bae7607e9e4e8be4dcc26c6",
+    "url": "/beans/static/media/github.766603df.svg"
+  },
+  {
+    "revision": "b513cfee0db7d2c0965f",
+    "url": "/beans/static/css/main.1cf1f539.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/beans/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "337f7d55587684801861",
+    "url": "/beans/static/js/1.337f7d55.chunk.js"
+  },
+  {
+    "revision": "da5fa8ab74261ee98dfa",
+    "url": "/beans/static/js/runtime~main.da5fa8ab.js"
+  },
+  {
+    "revision": "9aa4f7b7e63f490a4b71e9e172cd973f",
+    "url": "/beans/static/media/icons.9aa4f7b7.svg"
+  },
+  {
+    "revision": "5608be7aa8351c27bc295136efdcf3fa",
+    "url": "/beans/static/media/linkedin.5608be7a.svg"
+  },
+  {
+    "revision": "b513cfee0db7d2c0965f",
+    "url": "/beans/static/js/main.b513cfee.chunk.js"
+  },
+  {
+    "revision": "eda2e27876aa4693a67d03fd78a83367",
+    "url": "/beans/static/media/twitter.eda2e278.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/beans/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/beans/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/beans/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/beans/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "337f7d55587684801861",
+    "url": "/beans/static/css/1.bbb992e5.chunk.css"
+  },
+  {
+    "revision": "cd53f510ff3ba38343f3a71d06f672a5",
+    "url": "/beans/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.93965f4add477a43376a91ec24d87140.js"
+  "/beans/precache-manifest.310b625709d7fd11fa0178fed318f841.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/beans/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

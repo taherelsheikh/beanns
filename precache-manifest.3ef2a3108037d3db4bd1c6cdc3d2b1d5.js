d16d79f04425074298d077f644b4e605170ffce4
taher elsheikh <taher@gwmail.gwu.edu>
2019-07-21 21:38:41 -0700

    Updates

--- precache-manifest.3ef2a3108037d3db4bd1c6cdc3d2b1d5.js
+++ precache-manifest.3ef2a3108037d3db4bd1c6cdc3d2b1d5.js
@@ -4,12 +4,12 @@ self.__precacheManifest = [
     "url": "/static/media/github.766603df.svg"
   },
   {
-    "revision": "af7d2d290db69f856815",
-    "url": "/static/css/main.2d7047c3.chunk.css"
+    "revision": "02e8efb03d709c83b719",
+    "url": "/static/css/main.b5192588.chunk.css"
   },
   {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
     "revision": "1021a987ddfdbc1ea74f",
@@ -28,35 +28,35 @@ self.__precacheManifest = [
     "url": "/static/media/linkedin.5608be7a.svg"
   },
   {
-    "revision": "af7d2d290db69f856815",
-    "url": "/static/js/main.af7d2d29.chunk.js"
+    "revision": "02e8efb03d709c83b719",
+    "url": "/static/js/main.02e8efb0.chunk.js"
   },
   {
     "revision": "eda2e27876aa4693a67d03fd78a83367",
     "url": "/static/media/twitter.eda2e278.svg"
   },
   {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
     "revision": "b06871f281fee6b241d60582ae9369b9",
     "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
-  },
-  {
-    "revision": "912ec66d7572ff821749319396470bde",
-    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
-  },
-  {
     "revision": "1021a987ddfdbc1ea74f",
     "url": "/static/css/1.5b46f583.chunk.css"
   },
   {
-    "revision": "5f3ae3a7257533c5791af8acbd4ff410",
+    "revision": "5f74fdf0083e6d3c7efe2126005091ed",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.587aff68ff7109d584f19cd70028fee4.js"
+  "/precache-manifest.3ef2a3108037d3db4bd1c6cdc3d2b1d5.js"
 );
 
 workbox.clientsClaim();
